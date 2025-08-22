864692185e77c062d8c41bda8cd5368867315823
EricWithC04 <negativegame117@gmail.com>
2025-08-22 10:58:55 -0300

    feat: cards of technologies and tools

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -4,6 +4,59 @@ import Image from "next/image"
 import { Card, CardContent } from '@/components/ui/card'
 
 export const About = () => {
+
+    const cardStats = [
+        {
+            title: '50+',
+            desc: 'Proyectos completados'
+        },
+        {
+            title: '50+',
+            desc: 'Proyectos completados'
+        },
+        {
+            title: '50+',
+            desc: 'Proyectos completados'
+        },
+        {
+            title: '50+',
+            desc: 'Proyectos completados'
+        },
+    ]
+
+    const tech = [
+        {
+            title: 'Frontend',
+            items: [
+                'HTML',
+            ]
+        },
+        {
+            title: 'Backend',
+            items: [
+                'HTML',
+            ]
+        },
+        {
+            title: 'Mobile',
+            items: [
+                'HTML',
+            ]
+        },
+        {
+            title: 'Game Dev',
+            items: [
+                'HTML',
+            ]
+        },
+        {
+            title: 'Tools',
+            items: [
+                'HTML',
+            ]
+        },
+    ]
+
     return (
         <section className='py-24 relative flex flex-col items-center gap-12 bg-black' id="about">
             <div className='absolute inset-0 z-5 bg-neutral-900/50'></div>
@@ -37,31 +90,32 @@ export const About = () => {
                     />
                 </div>
             </div>
-            <div className='w-full px-24 grid lg:grid-cols-4 gap-8 z-10 [&>div]:bg-neutral-900/30'>
-                    <Card className='text-center py-12 border-1 border-neutral-800'>
-                        <CardContent className='space-y-2'>
-                            <h1 className='text-3xl font-semibold text-red-500'>50+</h1>
-                            <span className='text-md text-neutral-400'>Proyectos completados</span>
-                        </CardContent>
-                    </Card>
-                    <Card className='text-center py-12 border-1 border-neutral-800'>
-                        <CardContent className='space-y-2'>
-                            <h1 className='text-3xl font-semibold text-red-500'>50+</h1>
-                            <span className='text-md text-neutral-400'>Proyectos completados</span>
-                        </CardContent>
-                    </Card>
-                    <Card className='text-center py-12 border-1 border-neutral-800'>
-                        <CardContent className='space-y-2'>
-                            <h1 className='text-3xl font-semibold text-red-500'>50+</h1>
-                            <span className='text-md text-neutral-400'>Proyectos completados</span>
-                        </CardContent>
-                    </Card>
-                    <Card className='text-center py-12 border-1 border-neutral-800'>
-                        <CardContent className='space-y-2'>
-                            <h1 className='text-3xl font-semibold text-red-500'>50+</h1>
-                            <span className='text-md text-neutral-400'>Proyectos completados</span>
-                        </CardContent>
-                    </Card>
+            <div className='w-full px-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 z-10 [&>div]:bg-neutral-900/30'>
+                    {cardStats.map((stat, index) => (
+                        <Card key={index} className='text-center py-12 border-1 border-neutral-800'>
+                            <CardContent className='space-y-2'>
+                                <h1 className='text-3xl font-semibold text-red-500'>{stat.title}</h1>
+                                <span className='text-md text-neutral-400'>{stat.desc}</span>
+                            </CardContent>
+                        </Card>
+                    ))}
+            </div>
+            <div className='w-full flex flex-col py-8 gap-12 items-center z-10'>
+                <h1 className='text-2xl text-red-500 font-semibold'>Tecnologías y Herramientas</h1>
+                <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 px-24'>
+                    {tech.map((tool, index) => (
+                        <Card key={index} className='pt-6 pb-12 border-1 border-neutral-800 bg-neutral-900/30'>
+                            <CardContent className='space-y-2'>
+                                <h1 className='text-xl font-semibold text-red-500 mb-4'>{tool.title}</h1>
+                                <div>
+                                    {tool.items.map((item, index) => (
+                                        <Badge key={index} className='border-1 border-neutral-800'>{item}</Badge>
+                                    ))}
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
             </div>
         </section>
     )
