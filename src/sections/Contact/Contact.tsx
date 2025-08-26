39d1c9bd22a66faf4ed363ec850dbf19244d8ccc
EricWithC04 <negativegame117@gmail.com>
2025-08-26 10:32:38 -0300

    feat: last animations and title of page

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -2,22 +2,30 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { ContactForm } from '@/components/customs/ContactForm/ContactForm'
+import { Mail, Phone, MapPin, Send } from "lucide-react";
+import { SiGithub, SiLinkedin, SiInstagram } from 'react-icons/si';
 
 export const Contact = () => {
 
+    const appearanceAnimation = {
+        initial: { opacity: 0, y: 30 },
+        whileInView: { opacity: 1, y: 0 },
+        transition: { duration: 0.5 }
+    }
+
     const contactInfo = [
         {
-            icon: "",
+            icon: Mail,
             title: "Email",
             desc: "ericmerccado@gmail.com",
         },
         {
-            icon: "",
+            icon: Phone,
             title: "Teléfono",
             desc: "+54 9 370-4607556",
         },
         {
-            icon: "",
+            icon: MapPin,
             title: "Ubicación",
             desc: "Formosa, Argentina",
         },
@@ -25,21 +33,31 @@ export const Contact = () => {
 
     return (
         <section className='bg-black py-24 px-24 space-y-12' id="contact">
-            <div className='flex flex-col items-center gap-4'>
+            <motion.div 
+                className='flex flex-col items-center gap-4'
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+            >
                 <h1 className='text-4xl text-red-500 font-bold'>¡Trabajemos Juntos!</h1>
                 <p className='text-lg text-neutral-500 max-w-1/2 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eius eos quod sed laboriosam explicabo.</p>
-            </div>
+            </motion.div>
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 px-12'>
-                <div className='space-y-4'>
+                <motion.div 
+                    className='space-y-4'
+                    initial={{ opacity: 0, x: -50 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
                     <div className='space-y-4'>
                         <h1 className='text-2xl text-red-500 font-semibold'>Información de Contacto</h1>
                         <div className='flex flex-col gap-6'>
                             {contactInfo.map((item, index) => (
-                                <motion.div whileHover={{ scale: 1.05 }}>
-                                    <div key={index} className='relative group flex items-center py-2 px-4 gap-2 rounded-lg overflow-hidden hover:cursor-pointer'>
+                                <motion.div key={index} whileHover={{ scale: 1.05 }}>
+                                    <div className='relative group flex items-center py-2 px-4 gap-2 rounded-lg overflow-hidden hover:cursor-pointer'>
                                         <div className='absolute inset-0 group-hover:bg-neutral-500/20 duration-300 z-10'></div>
                                         <div className='w-10 h-10 bg-red-500/30 rounded-lg flex items-center justify-center'>
-                                            {item.icon}
+                                            <item.icon color="#ee4544" size={24}/>
                                         </div>
                                         <div>
                                             <h3 className='text-lg text-white font-semibold'>{item.title}</h3>
@@ -53,24 +71,23 @@ export const Contact = () => {
                     <div className='space-y-4'>
                         <h3 className='text-xl text-red-500 font-semibold mb-4'>Sígueme en redes</h3>
                         <div className='flex gap-4'>
-                            <motion.div 
-                                className='h-12 w-12 rounded-lg bg-neutral-500/50 hover:bg-red-500 hover:cursor-pointer'
-                                whileHover={{ scale: 1.1 }}
-                            ></motion.div>
-                            <motion.div 
-                                className='h-12 w-12 rounded-lg bg-neutral-500/50 hover:bg-red-500 hover:cursor-pointer'
-                                whileHover={{ scale: 1.1 }}
-                            ></motion.div>
-                            <motion.div 
-                                className='h-12 w-12 rounded-lg bg-neutral-500/50 hover:bg-red-500 hover:cursor-pointer'
-                                whileHover={{ scale: 1.1 }}
-                            ></motion.div>
+                            {[SiGithub, SiLinkedin, SiInstagram].map((Icon, index) => (
+                                <motion.div
+                                    key={index} 
+                                    className='h-12 w-12 flex items-center justify-center rounded-lg bg-neutral-500/50 hover:bg-red-500 hover:cursor-pointer'
+                                    whileHover={{ scale: 1.1 }}
+                                ><Icon color="white" size={24} /></motion.div>
+                            ))}
                         </div>
                     </div>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, x: 50 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
                     <ContactForm />
-                </div>
+                </motion.div>
             </div>
         </section>
     )
